f5343de6ddfb9a17d878e08e4ee4c93d355ba9e9
nitin chandekar <nitin.chandekar@stltech.in>
2020-12-22 22:18:32 +0530

    First...

---
+++ build/precache-manifest.acdbeef7be2caa5bd8c711d0e3788075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468fc11691a9eed24b4185a9ada9ad4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed499e955b8bb6035dc4",
+    "url": "/static/css/main.4371d45e.chunk.css"
+  },
+  {
+    "revision": "1e4b5701f57379159d2c",
+    "url": "/static/js/2.21992611.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.21992611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed499e955b8bb6035dc4",
+    "url": "/static/js/main.ceb335a0.chunk.js"
+  },
+  {
+    "revision": "ee1cca4d832c104b6e3e",
+    "url": "/static/js/runtime-main.a27f7325.js"
+  }
+]);
